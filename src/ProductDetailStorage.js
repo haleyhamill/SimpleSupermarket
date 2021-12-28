6444df6f7daf2ac02dce680855cfc1cca56dd25d
Haley Hamill <hamillhr@gmail.com>
2021-12-28 09:26:09 -0500

    migrated app into new repo

---
+++ src/ProductDetailStorage.js
@@ -0,0 +1,10 @@
+import './index.css';
+import React from "react";
+
+export default function ProductDetailStorage({ storage }) {
+  return (
+    <p>
+      <strong>Storage instructions:</strong> {storage}
+    </p>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,79 @@
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import {render} from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import About from "./About.js";
+import Products from "./Products.js";
+import ProductDetails from "./ProductDetails.js";
+import Cart from "./Cart.js";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function handleProductDelete(id) {
+    const updatedCart = cart.filter((product) => product.id !== id);
+    setCart(updatedCart);
+  }
+
+  function handleProductAdd(newProduct) {
+    // check if item exists
+    const existingProduct = cart.find(
+      (product) => product.id === newProduct.id
+    );
+    if (existingProduct) {
+      // increase quantity
+      const updatedCart = cart.map((product) => {
+        if (product.id === newProduct.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+        return product;
+      });
+      setCart(updatedCart);
+    } else {
+      // product is new to the cart
+      setCart([
+        ...cart,
+        {
+          ...newProduct,
+          quantity: 1,
+        },
+      ]);
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      <Navbar cart={cart} />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/products">
+            <Products
+              cart={cart}
+              onProductAdd={handleProductAdd}
+              onProductDelete={handleProductDelete}
+            />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails onProductAdd={handleProductAdd} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+render(<App />, document.getElementById("root"));
