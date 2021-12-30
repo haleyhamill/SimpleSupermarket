01393a62b680f895ddf8776f31b7dc45a6750a94
Haley Hamill <hamillhr@gmail.com>
2021-12-29 21:40:28 -0500

    adding context

---
+++ src/AppContext.js
@@ -0,0 +1,40 @@
+import React, {createContext} from "react";
+
+const context = createContext();
+
+function AppProvider() {
+    const [cart, setCart] = useState();
+
+    function handleProductDelete(id) {
+        const updatedCart = cart.filter((product) => product.id !== id);
+        setCart(updatedCart);
+      }
+    
+      function handleProductAdd(newProduct) {
+        // check if item exists
+        const existingProduct = cart.find(
+          (product) => product.id === newProduct.id
+        );
+        if (existingProduct) {
+          // increase quantity
+          const updatedCart = cart.map((product) => {
+            if (product.id === newProduct.id) {
+              return {
+                ...product,
+                quantity: product.quantity + 1,
+              };
+            }
+            return product;
+          });
+          setCart(updatedCart);
+        } else {
+          // product is new to the cart
+          setCart([
+            ...cart,
+            {
+              ...newProduct,
+              quantity: 1,
+            },
+          ]);
+        }
+}
