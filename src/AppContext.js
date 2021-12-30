e269213d4fc75a793ff04d354bcd2b6d9fca1ef3
Haley Hamill <hamillhr@gmail.com>
2021-12-29 21:43:06 -0500

    added context

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,40 +1,53 @@
-import React, {createContext} from "react";
+import React, { createContext } from "react";
 
 const context = createContext();
 
-function AppProvider() {
+function AppProvider(props) {
     const [cart, setCart] = useState();
 
     function handleProductDelete(id) {
         const updatedCart = cart.filter((product) => product.id !== id);
         setCart(updatedCart);
-      }
-    
-      function handleProductAdd(newProduct) {
+    }
+
+    function handleProductAdd(newProduct) {
         // check if item exists
         const existingProduct = cart.find(
-          (product) => product.id === newProduct.id
+            (product) => product.id === newProduct.id
         );
         if (existingProduct) {
-          // increase quantity
-          const updatedCart = cart.map((product) => {
-            if (product.id === newProduct.id) {
-              return {
-                ...product,
-                quantity: product.quantity + 1,
-              };
-            }
-            return product;
-          });
-          setCart(updatedCart);
+            // increase quantity
+            const updatedCart = cart.map((product) => {
+                if (product.id === newProduct.id) {
+                    return {
+                        ...product,
+                        quantity: product.quantity + 1,
+                    };
+                }
+                return product;
+            });
+            setCart(updatedCart);
         } else {
-          // product is new to the cart
-          setCart([
-            ...cart,
-            {
-              ...newProduct,
-              quantity: 1,
-            },
-          ]);
+            // product is new to the cart
+            setCart([
+                ...cart,
+                {
+                    ...newProduct,
+                    quantity: 1,
+                },
+            ]);
         }
+    }
+
+    const value = {
+        cart: cart,
+        onProductAdd: handleProductAdd,
+        onProductDelete: handleProductDelete,
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
 }
