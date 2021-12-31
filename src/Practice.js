c04bce8db0d20ba8e330b7dea5a0f8c661708cb4
Haley Hamill <hamillhr@gmail.com>
2021-12-30 21:58:37 -0500

    added context

--- src/Practice.js
+++ src/Practice.js
@@ -1,23 +1,8 @@
-import { useEffect } from "react";
+import React, {useContext} from "react";
+import {ThemeContext} from "./ThemeContext.js";
 
-function CurrencySelector() {
-    const [currency, setCurrency] = useState("");
+function Button(props) {
+    const theme = useContext(ThemeContext);
 
-    useEffect(() => {
-        console.log(currency);
-    }, []);
-
-    function handleCurrencyChange(event) {
-        setCurrency(event.target.value);
-    }
-
-    return <>
-    <h3>Select currency</h3>
-    <select onChange={handleCurrencyChange}>
-        <option value="usd">USD</option>
-        <option value="eur">EUR</option>
-        <option value="cad">CAD</option>
-    </select>
-    <h1>{currency}</h1>
-    </>;
+    return null;
 }
