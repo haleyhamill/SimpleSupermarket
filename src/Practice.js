cc40c07f530d98e1e13b0f31f06d54dcdc1fff20
Haley Hamill <hamillhr@gmail.com>
2022-01-01 20:40:31 -0500

    added provider

--- src/Practice.js
+++ src/Practice.js
@@ -1,40 +1,11 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import {render} from "react-dom";
+import Counter from "./counter.js";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: {
-    value: 0,
-  },
-  reducers: {
-    // TODO
-    incrementBy: (state, action) => {
-      state.value += action.payload
-    },
-  },
-});
+function App() {
+    return <Counter />;
+}
 
-const store = configureStore({
-  reducer: counterSlice.reducer,
-});
-
-const { incrementBy } = counterSlice.actions;
-
-const counterValue = document.querySelector("#counter-value");
-
-store.subscribe(() => {
-    const newState = store.getState();
-    console.log("New state", newState);
-    counterValue.textContent = newState.value;
-});
-
-document.querySelector("#add-one").addEventListener("click", () => {
-    store.dispatch(incrementBy(1));
-});
-
-document.querySelector("#add-five").addEventListener("click", () => {
-    store.dispatch(incrementBy(5));
-});
-
-document.querySelector("#add-ten").addEventListener("click", () => {
-    store.dispatch(incrementBy(10));
-});
+render(<Provider store={store}><App /></Provider>, document.querySelector("#react-root"));
