e411caad99263a04ae26edd96b72f4822b3aaa05
Haley Hamill <hamillhr@gmail.com>
2022-01-01 19:47:14 -0500

    used immer

--- src/Practice.js
+++ src/Practice.js
@@ -1,40 +1,31 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState = { value: 0 };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      // TODO: handle the "increment" action
+      state.value += 1
+    },
+  },
+});
 
-const counterReducer = (state = initialState, action) => {
-    console.log("Current state", state);
-    console.log("Received action", action);
-
-    if (action.type === "counter/increment") {
-        return {
-            value: state.value + 1 // important: do NOT mutate the state.
-        };
-    } else if (action.type === "counter/decrement") {
-        return {
-            value: state.value - 1
-        }
-    } else if (action.type === "counter/reset") {
-        return {
-            value: 0
-        };
-    }
-
-    return state; // return the state as is (in all other cases)
-};
-
-const store = createStore(counterReducer);
-
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 /*********************************************
  * The code below has NOT been explained yet *
  *         feel free to take a peek          *
  *    but don't worry too much about it!     *
  *********************************************/
+const { increment } = counterSlice.actions;
+
 const counterValue = document.querySelector("#counter-value");
 const addButton = document.querySelector("#add-button");
-const subButton = document.querySelector("#sub-button");
-const resetButton = document.querySelector("#reset-button");
 
 store.subscribe(() => {
     const newState = store.getState();
@@ -43,13 +34,5 @@ store.subscribe(() => {
 });
 
 addButton.addEventListener("click", () => {
-    store.dispatch({ type: "counter/increment" });
-});
-
-subButton.addEventListener("click", () => {
-    store.dispatch({ type: "counter/decrement" });
-});
-
-resetButton.addEventListener("click", () => {
-    store.dispatch({ type: "counter/reset" });
+    store.dispatch(increment());
 });
