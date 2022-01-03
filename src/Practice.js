eb9bb1b52336bd16bef32d9d273408ee6933c81c
Haley Hamill <hamillhr@gmail.com>
2022-01-02 23:17:47 -0500

    learning substring

--- src/Practice.js
+++ src/Practice.js
@@ -1,11 +1,2 @@
-/**
- * @param {string} str
- */
- function getCharCount(str) {
-    return str.length;
-}
-
-// Sample usage - do not modify
-console.log(getCharCount("Sam")); // 3
-console.log(getCharCount("Alex 123")); // 8
-console.log(getCharCount("Charley is here")); // 15
+const language = "JavaScript";
+language.substring(1, 4);
