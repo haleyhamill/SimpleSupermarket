09d0db82ae2683b6f871af8903a911742dc17b90
Haley Hamill <hamillhr@gmail.com>
2022-01-12 17:48:41 -0500

    added customer info class

--- src/Practice.js
+++ src/Practice.js
@@ -1,7 +1,26 @@
+// class definition
 class User {
-    constructor(firstName, lastName, age) {
+    constructor(firstName, lastName, preF, age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.preF = preF;
         this.age = age;
     }
+
+    getFullName() {
+        return `${this.preF}. ${this.firstName} ${this.lastName}`
+    }
+
+    canVote() {
+        return this.age >= 18;
+    }
 }
+
+
+// Sample usage - do not modify
+const user1 = new User("Sam", "Doe", "Mrs", 20);
+console.log(user1.getFullName()); // "Mrs. Sam Doe"
+console.log(user1.canVote()); // true
+const user2 = new User("Alex", "Green", "Mr", 17);
+console.log(user2.getFullName()); // "Mr. Alex Green"
+console.log(user2.canVote()); // false
