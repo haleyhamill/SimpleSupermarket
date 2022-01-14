f5afbb64b88a451beba18328760926826809601c
Haley Hamill <hamillhr@gmail.com>
2022-01-14 15:26:14 -0500

    added callback

--- src/Practice.js
+++ src/Practice.js
@@ -1,23 +1,3 @@
-import React, {Component} from "react";
-
-class Counter extends Component {
-    constructor(props) {
-        super(props) {
-            this.state = {
-                counter: 1
-            };
-        }
-
-        componentDidMount() {
-            console.log("Component rendered (first time)")
-        }
-
-        handleClick(event) {
-            this.setState({counter: this.state.counter + 1});
-        }
-
-        render() {
-            return <button onClick={this.handleClick.bind(this)}>Add 1</button>
-        }
-    }
-}
+setTimeout(() => {
+    console.log("Hello World! Delayed");
+}, 2000);
