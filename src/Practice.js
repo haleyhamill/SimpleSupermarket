d3d7f448607778069aec3889c9f59cdb6deca53c
Haley Hamill <hamillhr@gmail.com>
2022-01-16 16:11:05 -0500

    add loader

--- src/Practice.js
+++ src/Practice.js
@@ -1,10 +1,11 @@
-import {getWeatherDescription} from "./weather.js";
-
-/**
- * @param {string} cityName
- */
-const logWeatherDescription = cityName => {
-    getWeatherDescription(cityName).then(data => {
+startLoader();
+getWeatherIn("Amsterdam")
+    .then(data => {
         console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(() => {
+        stopLoader();
     });
-}
