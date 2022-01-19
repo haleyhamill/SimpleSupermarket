8f73ca5bb64de3aa3b694fb10adef1ae03ef5af7
Haley Hamill <hamillhr@gmail.com>
2022-01-19 11:41:23 -0500

    working with fetch

--- src/Practice.js
+++ src/Practice.js
@@ -1,6 +1,32 @@
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+const fakeFetch = endpoint => {
+    // TODO: implement fake fetch
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        departed: false,
+        delayed: true
+      });
+    }, 1000)
+
+    if (endpoint != "flight-status") {
+      reject("endpoint not supported")
     }
+  })
 }
+
+// Sample usage - do not modify
+fakeFetch("flight-status")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+fakeFetch("user-details")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
