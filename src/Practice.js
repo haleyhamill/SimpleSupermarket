d60c531f33baa31b3b73cd48d8ee195941887466
Haley Hamill <hamillhr@gmail.com>
2022-01-19 11:45:38 -0500

    working on fetch

--- src/Practice.js
+++ src/Practice.js
@@ -1,32 +1,13 @@
-const fakeFetch = endpoint => {
-    // TODO: implement fake fetch
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        departed: false,
-        delayed: true
-      });
-    }, 1000)
-
-    if (endpoint != "flight-status") {
-      reject("endpoint not supported")
-    }
-  })
+// TODO: implement failAfter(milliseconds)
+const failAfter = milliseconds => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`You asked me to fail after ${milliseconds}ms and I did!`)
+        }, milliseconds);
+    })
 }
 
 // Sample usage - do not modify
-fakeFetch("flight-status")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-fakeFetch("user-details")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+failAfter(1500).catch(error => {
+    console.error(error); // "You asked me to fail after 1500ms and I did!"
+});
