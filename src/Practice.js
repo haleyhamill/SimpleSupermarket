dd8007cae811fbd526106c7dab6c19f684767fbf
Haley Hamill <hamillhr@gmail.com>
2022-01-19 18:01:31 -0500

    added fetch skeleton

--- src/Practice.js
+++ src/Practice.js
@@ -1,13 +1,9 @@
-// TODO: implement failAfter(milliseconds)
-const failAfter = milliseconds => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(`You asked me to fail after ${milliseconds}ms and I did!`)
-        }, milliseconds);
+fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
     })
-}
-
-// Sample usage - do not modify
-failAfter(1500).catch(error => {
-    console.error(error); // "You asked me to fail after 1500ms and I did!"
-});
+    .catch(error => {
+        // handle network errors
+        console.error(error);
+    });
