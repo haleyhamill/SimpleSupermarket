99c8bfc0bf8d81f3d555e9647a403c4049fe2868
Haley Hamill <hamillhr@gmail.com>
2022-01-24 23:22:48 -0500

    corrected grammar

--- src/Products.js
+++ src/Products.js
@@ -1,10 +1,10 @@
-import './index.css';
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import Product from "./Product.js";
 import useFetch from "./useFetch.js";
 import Loader from "./Loader.js";
 
-export default function Products(props) {
+export default function Products(prop) {
   const [products, setProducts] = useState([]);
   const { get, loading } = useFetch(
     "https://react-tutorial-demo.firebaseio.com/"
