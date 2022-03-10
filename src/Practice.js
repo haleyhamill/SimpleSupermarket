60609cca69e7619f7fa0f89dcf77900a6f646329
Haley Hamill <hamillhr@gmail.com>
2022-03-09 22:44:44 -0500

    small update

--- src/Practice.js
+++ src/Practice.js
@@ -3,9 +3,8 @@ import FetchWrapper from "./fetch-wrapper.js";
 const API = new FetchWrapper("URL");
 
 const checkForNewNotifications = () => {
-    API.get("/notifications/new.json")
-    .then(data => {
-        console.log(data);
-        showNewNotifications(data.count);
-    });
-}
+  API.get("/notifications/new.json").then((data) => {
+    console.log(data);
+    showNewNotifications(data.count);
+  });
+};
